0c81f75afb712ef125894b6a2861708a8ee00bbd
Scott Little <Scott Little>
2017-07-23 03:43:12 -0600

    add images for mobile

--- src/App.js
+++ src/App.js
@@ -24,13 +24,15 @@ class App extends Component {
         <div className="App-description">
           <h1>Factorio-Tiles</h1>
           <p>This is a potential solution to the problems presented in <a href="https://www.factorio.com/blog/post/fff-199">FFF-199</a>.</p>
+          <p>Here is a gif of the demo for those of you on mobile:</p>
+          <img src="/images/demo.gif"></img>
           <h2>Instructions</h2>
           <p>Left click to place grass. <br/> Right click to place water. <br/> Scroll to zoom. </p>
           <h3>Premise</h3>
           <p>Tiles in factorio currently represent the <em>transition</em> between multiple types of terrain (e.g.: grass to water is a little cliff). Each grid square is of a single terrain type. This means that <strong>tiles should be rendered in between grid squares</strong>. This demo has shifted the tiles to be rendered on the intersections of the grid, instead of the grid squares themselves. The marching squares algorithm can be used to determine which tile should be rendered.</p>
           <p>tl;dr; Use marching squares</p>
           <h3>Note</h3>
-          <p>This demo uses mostly original tiles from the game, with the exception of <a href="images/LWWL.png">this tile</a> and <a href="images/WLLW.png">this tile</a>.</p>
+          <p>This demo uses mostly original tiles from the game, with the exception of these tiles: <br/><img src="images/LWWL.png" /> <img src="images/WLLW.png" /></p>
           <h3>See Also</h3>
           <p><a href="https://www.reddit.com/r/factorio/comments/6nlzzs/my_idea_about_friday_facts_199/">Reddit: "My idea about Friday Facts #199"</a> posted by <a href="https://www.reddit.com/user/Dkluwe">Dkluwe</a></p>
         </div>
